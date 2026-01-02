314fd25236456fd9abdd62743b8f5d8e7cc31910
julio6745 <juliogonzalez6745@gmail.com>
2026-01-02 13:12:57 +0100

    Update settings.js

--- data/settings.js
+++ data/settings.js
@@ -1,13 +1,8 @@
 module.exports = {
     uiPort: process.env.PORT || 1880,
-
-    // Editor sin autenticación
     adminAuth: null,
-
-    // Dashboard sin autenticación
     httpNodeAuth: null,
     httpStaticAuth: null,
-
     functionGlobalContext: {}
 };
 
