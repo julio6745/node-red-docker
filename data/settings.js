f8af993e316ffcbd2ef19c52d855f15ad9dc0d84
julio6745 <juliogonzalez6745@gmail.com>
2026-01-02 12:41:18 +0100

    Update settings.js

--- data/settings.js
+++ data/settings.js
@@ -7,7 +7,8 @@ module.exports = {
         type: "credentials",
         users: [{
             username: process.env.NODE_RED_USERNAME || "julio",
-            password: process.env.NODE_RED_PASSWORD || "julio",
+            password: "$2b$08$8p0u8k8VnX0pZqVgZ8pE8u7qJxJgk1m6u3Q0J7p4q1Y8xW8xqf1yW",
+",
             permissions: "*"
         }]
     },
@@ -15,13 +16,12 @@ module.exports = {
     // Autenticación para los endpoints HTTP (incluye /ui)
     httpNodeAuth: {
         user: process.env.NODE_RED_USERNAME || "julio",
-        pass: process.env.NODE_RED_PASSWORD || "julio"
-    },
+        pass: "$2b$08$8p0u8k8VnX0pZqVgZ8pE8u7qJxJgk1m6u3Q0J7p4q1Y8xW8xqf1yW"
 
     // Autenticación para archivos estáticos (si los hubiera)
     httpStaticAuth: {
         user: process.env.NODE_RED_USERNAME || "julio",
-        pass: process.env.NODE_RED_PASSWORD || "julio"
+        pass: "$2b$08$8p0u8k8VnX0pZqVgZ8pE8u7qJxJgk1m6u3Q0J7p4q1Y8xW8xqf1yW"
     },
 
     // Contexto global (puedes añadir variables aquí si las necesitas)
