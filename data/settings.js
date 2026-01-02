77c9b72434c1e41197cbf710be58d80abc92bd6e
julio6745 <juliogonzalez6745@gmail.com>
2026-01-02 13:02:56 +0100

    Update settings.js

--- data/settings.js
+++ data/settings.js
@@ -1,17 +1,10 @@
 module.exports = {
     uiPort: process.env.PORT || 1880,
 
-    // Solo el editor protegido
-    adminAuth: {
-        type: "credentials",
-        users: [{
-            username: "julio",
-            password: "$2b$08$8p0u8k8VnX0pZqVgZ8pE8u7qJxJgk1m6u3Q0J7p4q1Y8xW8xqf1yW",
-            permissions: "*"
-        }]
-    },
+    // Editor sin autenticación
+    adminAuth: null,
 
-    // Dashboard sin contraseña
+    // Dashboard sin autenticación
     httpNodeAuth: null,
     httpStaticAuth: null,
 
