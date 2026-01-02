77474e9f1d885ea167d715d2d0c93c2183fe56ca
julio6745 <juliogonzalez6745@gmail.com>
2026-01-02 13:45:12 +0100

    Update settings.js

--- data/settings.js
+++ data/settings.js
@@ -6,7 +6,7 @@ module.exports = {
         type: "credentials",
         users: [{
             username: "julio",
-            password: "$2b$08$8p0u8k8VnX0pZqVgZ8pE8u7qJxJgk1m6u3Q0J7p4q1Y8xW8xqf1yW",
+            password: "$2b$08$Cj8qv8uJx6pG8wFqVq9uUu8G1xJxJgk1m6u3Q0J7p4q1Y8xW8xqf1y",
             permissions: "*"
         }]
     },
