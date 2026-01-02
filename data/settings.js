f586741c35c0bbe8e827393ba94b46439bba618c
julio6745 <juliogonzalez6745@gmail.com>
2026-01-02 13:49:58 +0100

    Update settings.js

--- data/settings.js
+++ data/settings.js
@@ -1,18 +1,7 @@
 module.exports = {
     uiPort: process.env.PORT || 1880,
-    uiHost: "0.0.0.0",
-
-    adminAuth: {
-        type: "credentials",
-        users: [{
-            username: "julio",
-            password: "$2b$08$uH0q4xW8pZqVgZ8pE8u7qO3xJgk1m6u3Q0J7p4q1Y8xW8xqf1yW2",
-            permissions: "*"
-        }]
-    },
-
+    adminAuth: null,
     httpNodeAuth: null,
     httpStaticAuth: null,
-
     functionGlobalContext: {}
 };
